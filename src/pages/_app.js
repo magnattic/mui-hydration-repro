6534bed34fdc068b2462b2a1e63e5612ad636602
magnattic <atticae@gmail.com>
2025-05-20 12:42:34 +0200

    minimal repro

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import { AppCacheProvider } from "@mui/material-nextjs/v15-pagesRouter";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps, ...props }) {
+	return (
+		<AppCacheProvider {...props}>
+			<Component {...pageProps} />
+		</AppCacheProvider>
+	);
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import {
+	DocumentHeadTags,
+	createEmotionCache,
+	documentGetInitialProps,
+} from "@mui/material-nextjs/v15-pagesRouter";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default function Document(props) {
+	return (
+		<Html lang="en">
+			<Head>
+				<DocumentHeadTags {...props} />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
+
+Document.getInitialProps = async (ctx) => {
+	const finalProps = await documentGetInitialProps(ctx, {
+		emotionCache: createEmotionCache({ enableCssLayer: true }),
+	});
+	return finalProps;
+};
